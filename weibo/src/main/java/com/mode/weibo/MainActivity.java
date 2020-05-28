9ea87be65aa88a5aad56aa14f0ca74f7dc34d2b9
NKW <2314504058@qq.com>
2020-05-29 01:35:21 +0800

    添加其他button

--- weibo/src/main/java/com/mode/weibo/MainActivity.java
+++ weibo/src/main/java/com/mode/weibo/MainActivity.java
@@ -5,42 +5,73 @@ import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatEditText;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppCompatEditText mEt;
-    private AppCompatButton   mBtn;
+    public static final String            TAG = "MainActivity";
+    private             AppCompatEditText mEt;
+    private             AppCompatButton   mBtnCoverUrl;
+    private             AppCompatButton   mBtnStartServer;
+    private             AppCompatButton   mBtnStopServer;
+    private             AppCompatActivity mActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mActivity = this;
         setContentView(R.layout.activity_main);
         mEt = findViewById(R.id.et);
-        mBtn = findViewById(R.id.btn);
-        mBtn.setOnClickListener(new View.OnClickListener() {
+        mBtnCoverUrl = findViewById(R.id.btn_covert_url);
+        mBtnCoverUrl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 v.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        if (mBtn == null) {
+                        if (mBtnCoverUrl == null) {
                             return;
                         }
-                        Toast.makeText(MainActivity.this,"开始转换",Toast.LENGTH_LONG).show();
+                        Toast.makeText(mActivity, "开始转换", Toast.LENGTH_LONG).show();
                         String string = mEt.getText().toString();
                         if (!TextUtils.isEmpty(string)) {
                             Intent intent = new Intent();
                             intent.putExtra("UrlListData", string);
                             intent.setAction("com.wb.data.url");
-                            MainActivity.this.sendBroadcast(intent);
+                            mActivity.sendBroadcast(intent);
                         }
                     }
                 }, 30 * 1000);
+            }
+        });
+
+        mBtnStartServer = findViewById(R.id.btn_start_server);
+        mBtnStartServer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mActivity, "服务器启动", Toast.LENGTH_LONG).show();
+            }
+        });
+        mBtnStopServer = findViewById(R.id.btn_stop_server);
+        mBtnStopServer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mActivity, "服务器停止", Toast.LENGTH_LONG).show();
             }
         });
     }
